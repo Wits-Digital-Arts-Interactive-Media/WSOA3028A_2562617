b0190983215b594372c7e8884a81c96c9a35aa33
Zandile Gebuza <2562617@students.wits.ac.za>
2024-04-23 15:57:43 +0200

    Corrections

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,4 @@
-const menuItems = 
-[
+const menuItems = [
     {name: "Home", href: "/index.html"},
     {name: "Blog Posts", href: "/blogs/index.html"},
     {name: "Design Process", href: "/design/index.html"},
@@ -7,29 +6,25 @@ const menuItems =
     {name: "Portfolio", href: "/portfolio/index.html"},
 ];
 
-export function initialise(currentPage) 
-{
-    const nav = document.querySelector("header > nav");
-    const ul = document.createElement("ul");
+export function initialise (currentPage) {
+const nav = document.querySelector ("header > nav");
+const ul = document.createElement ("ul");
 
-    for (let menuItem of menuItems) 
-    {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        if (currentPage != menuItem.name) 
-        {
-            a.innerText = menuItem.name;
-            a.setAttribute("href", menuItem.href);
-        } else 
-        
-        {
-            a.innerText = menuItem.name;
-            a.setAttribute("href", currentPage === "Home" ? "/index.html" : `/${currentPage.toLowerCase()}/index.html`);
-        }
-        li.appendChild(a);
-        ul.appendChild(li);
+for (let menuItem of menuItems) {
+    const li = document.createElement ("li");
+    const a = document.createElement ("a");
+    a.innerText = menuItem.name; 
+    a.setAttribute ("href", menuItem.href);
+
+    if (currentPage === menuItem.name) {
+        li.innerText = menuItem.name;
+    } 
+    else {
+        li.appendChild (a);
     }
-    nav.appendChild(ul);
+    ul.appendChild (li);
+} 
+nav.appendChild (ul);
 }
 
 
