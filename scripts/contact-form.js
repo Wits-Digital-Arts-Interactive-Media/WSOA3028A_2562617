f2faa30f9c5974360633ba8e370703216eff43d4
Zandile Gebuza <2562617@students.wits.ac.za>
2024-06-19 17:40:41 +0200

    Edited Design Pages

--- scripts/contact-form.js
+++ scripts/contact-form.js
@@ -1,9 +1,6 @@
 //Contact Form
 
-/*The user can contact me by writing a message into the text field from my Portfolio page.
-  This is a default response. I don't know how to use PHP code yet so I can't actually receive the message.
-  I also did not want to waste time learning how to do it when it is not required for the exam. 
-  However, I will learn how to use PHP when I start using my website outside of the course.*/
+/*The user can contact me by writing a message into the text field from my Portfolio page.*/
 
 //This is the function for the Submit button. When it is clicked, the code will check if the user wrote something.
 function submitForm() {
@@ -15,6 +12,7 @@ function submitForm() {
         responseMessage.textContent = "Please enter a message.";
         responseMessage.style.color = "red";
     } else {
+        //This is the response that the user will get after submitting the form.
         responseMessage.textContent = "Thank you for your message!";
         responseMessage.style.color = "purple";
 
